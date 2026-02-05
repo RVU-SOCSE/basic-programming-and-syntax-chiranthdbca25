5de8793c5d65be43a4299373e47bb0eb62c35fdc
Chiranth D <chiranthdbca25@rvu.edu.in>
2026-02-05 10:19:24 +0530

    Add files via upload

---
+++ Program to find the largest of three numbers.py
@@ -0,0 +1,10 @@
+#1RUA25BCA0022_CHIRANTH D
+a=int(input("ENTER AN FIRST NUMBER:"))
+b=int(input("ENTER AN SECOND NUMBER:"))
+c=int(input("ENTER AN THIRD NUMBER:"))
+if a>b and a>c:
+    print("A is greater")
+elif b>a and b>c:
+    print("B is greater")
+else:
+    print("C is greater")
